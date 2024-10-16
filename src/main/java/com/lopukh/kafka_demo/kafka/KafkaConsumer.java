6106888e56f5c87a08e8095b8c9f5a2786aca101
Pavel Lopukhov <lopukhovpavel@gmail.com>
2024-10-16 18:51:52 +0300

    from second_brench

--- src/main/java/com/lopukh/kafka_demo/kafka/KafkaConsumer.java
+++ src/main/java/com/lopukh/kafka_demo/kafka/KafkaConsumer.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumer {
-
+    //git second_branch commit
     @KafkaListener(topics = "kafka_test", groupId = "my_consumer")
     public void listen(String message){
         System.out.println("Received message: " + message);
